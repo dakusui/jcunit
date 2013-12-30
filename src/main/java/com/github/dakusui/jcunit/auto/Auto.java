6abf0dd6649869f5b83fc53770fedd8bfe34cb34
Hiroshi Ukai <dakusui@gmail.com>
2013-12-30 15:30:09 +0900

    Add a new rule implementation base.

--- src/main/java/com/github/dakusui/jcunit/auto/Auto.java
+++ src/main/java/com/github/dakusui/jcunit/auto/Auto.java
@@ -1,6 +1,13 @@
 package com.github.dakusui.jcunit.auto;
 
+import java.io.File;
+import java.lang.reflect.Field;
+
+import org.junit.rules.TestName;
+
+import com.github.dakusui.jcunit.core.Utils;
 import com.github.dakusui.jcunit.exceptions.JCUnitException;
+import com.github.dakusui.lisj.Basic;
 import com.github.dakusui.lisj.CUT;
 import com.github.dakusui.lisj.Context;
 import com.github.dakusui.lisj.FormResult;
@@ -13,6 +20,20 @@ public class Auto extends BaseFunc {
 	 */
 	private static final long serialVersionUID = -2402014565260025741L;
 
+	/**
+	 * This function takes one and only one parameter, which is a name of a field.
+	 *  
+	 * @see com.github.dakusui.lisj.BaseForm#checkParams(java.lang.Object)
+	 */
+	@Override
+	protected Object checkParams(Object params) {
+		super.checkParams(params);
+		if (Basic.length(params) != 2) throw new IllegalArgumentException();
+		Utils.checknull(Basic.get(params, 0));
+		Utils.checknull(Basic.get(params, 1));
+		return params;
+	}
+
 	@Override
 	protected FormResult evaluateLast(
 			Context context,
@@ -21,34 +42,53 @@ public class Auto extends BaseFunc {
 			)
 			throws JCUnitException, CUT {
 		FormResult ret = lastResult;
-		if (isAlreadyStored()) {
-			store();
+		/*
+		 * We can use the first and second elements without a check since 'checkParams'
+		 * method guarantees that the array has two and only two elements. 
+		 */
+		Object obj       = evaluatedParams[0].toString();
+		String fieldName = evaluatedParams[1].toString();
+		if (isAlreadyStored(obj, fieldName)) {
+			store(obj, fieldName);
+			ret.value(false);
 		} else {
-			Object previous = load();
-			Object current = null;
-			verify(previous, current);
+			Object previous = load(obj, fieldName);
+			Object current = get(obj, fieldName);
+			ret.value(verify(previous, current));
 		}
 		return ret;
 	}
 
-	private void verify(Object previous, Object current) {
-		// TODO Auto-generated method stub
+	private File baseDir() {
+		return null;
 	}
 
-	private Object load() {
+	private File fileForField(File baseDir, TestName name, Field out) {
+		return new File(baseDir, this.getClass().getCanonicalName() + "/" + name.getMethodName() + "/" + out.getName());
+	}
+	
+	private Object get(Object obj, String fieldName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	private void store() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private boolean isAlreadyStored() {
+	private boolean verify(Object previous, Object current) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	private Object load(Object obj, String fieldName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	private void store(Object obj, String fieldName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private boolean isAlreadyStored(Object obj, String fieldName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
