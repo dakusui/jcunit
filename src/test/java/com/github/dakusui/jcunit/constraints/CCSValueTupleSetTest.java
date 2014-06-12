99061a2c7724a2178f9ad500ae7a271ffa05e9f7
dakusui <dakusui@gmail.com>
2014-06-13 06:48:56 +0900

    Add CCSValueTupleSetTest

---
+++ src/test/java/com/github/dakusui/jcunit/constraints/CCSValueTupleSetTest.java
@@ -0,0 +1,18 @@
+package com.github.dakusui.jcunit.constraints;
+
+import com.github.dakusui.jcunit.constraints.ccs.CCSValueTuple;
+
+/**
+ * Created by hiroshi on 6/11/14.
+ */
+public class CCSValueTupleSetTest {
+
+
+  CCSValueTuple<String, String> composeValueTuple1() {
+    return null;
+  }
+
+  CCSValueTuple<String, String> composeValueTuple2() {
+    return null;
+  }
+}
