7c8722f647f9163f339b39b1fd27a45364f7cc39
Hiroshi Ukai <dakusui@gmail.com>
2014-08-06 08:29:46 +0900

    Initial commit for fileter mechanism

--- src/main/java/com/github/dakusui/jcunit/core/Filter.java
+++ src/main/java/com/github/dakusui/jcunit/core/Filter.java
@@ -1,7 +1,4 @@
 package com.github.dakusui.jcunit.core;
 
-/**
- * Created by hiroshi on 14/08/06.
- */
 public @interface Filter {
 }
--- src/main/java/com/github/dakusui/jcunit/core/JCUnit.java
+++ src/main/java/com/github/dakusui/jcunit/core/JCUnit.java
@@ -15,6 +15,7 @@ import java.io.Serializable;
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,234 +24,216 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class JCUnit extends Suite {
-	private final ArrayList<Runner> runners = new ArrayList<Runner>();
+    private final ArrayList<Runner> runners = new ArrayList<Runner>();
 
-	/**
-	 * Only called reflectively by JUnit. Do not use programmatically.
-	 */
-	public JCUnit(Class<?> klass) throws Throwable {
-		super(klass, Collections.<Runner>emptyList());
-		try {
-			TupleGenerator tupleGenerator = TupleGeneratorFactory.INSTANCE
-					.createTupleGeneratorFromClass(klass);
-			Factors factors = tupleGenerator.getFactors();
-			int id;
-			TestCaseFilter filter = createTestCaseFilter(klass);
-			for (id = (int) tupleGenerator.firstId();
-			     id >= 0; id = (int) tupleGenerator.nextId(id)) {
-				if (filter.shouldBeExecuted(id)) {
-					runners.add(new JCUnitRunner(getTestClass().getJavaClass(),
-							id, TestCaseType.Generated, new LinkedList<Serializable>(),
-							factors,
-							tupleGenerator.get(id)));
-				}
-			}
-			id = (int) tupleGenerator.size();
-			ConstraintManager cm = tupleGenerator.getConstraintManager();
-			final List<LabeledTestCase> violations = cm.getViolations();
-			id = registerLabeledTestCases(
-					id,
-					filter,
-					factors,
-					violations,
-					TestCaseType.Custom);
-			if (hasParametersMethod()) {
-				registerLabeledTestCases(
-						id,
-						filter,
-						factors,
-						allCustomTestCases(),
-						TestCaseType.Violation);
-			}
-			ConfigUtils.checkEnv(runners.size() > 0, "No test to be run was found.");
-		} catch (JCUnitUserException e) {
-			e.setTargetClass(klass);
-			throw e;
-		}
-	}
+    /**
+     * Only called reflectively by JUnit. Do not use programmatically.
+     */
+    public JCUnit(Class<?> klass) throws Throwable {
+        super(klass, Collections.<Runner>emptyList());
+        try {
+            TupleGenerator tupleGenerator = TupleGeneratorFactory.INSTANCE
+                    .createTupleGeneratorFromClass(klass);
+            Factors factors = tupleGenerator.getFactors();
+            int id;
+            for (id = (int) tupleGenerator.firstId();
+                 id >= 0; id = (int) tupleGenerator.nextId(id)) {
+                runners.add(new JCUnitRunner(getTestClass().getJavaClass(),
+                        id, TestCaseType.Generated, new LinkedList<Serializable>(),
+                        factors,
+                        tupleGenerator.get(id)));
+            }
+            id = (int) tupleGenerator.size();
+            ConstraintManager cm = tupleGenerator.getConstraintManager();
+            final List<LabeledTestCase> violations = cm.getViolations();
+            id = registerLabeledTestCases(
+                    id,
+                    factors,
+                    violations,
+                    TestCaseType.Custom);
+            if (hasCustomTestCasesMethod()) {
+                registerLabeledTestCases(
+                        id,
+                        factors,
+                        allCustomTestCases(),
+                        TestCaseType.Violation);
+            }
+            ConfigUtils.checkEnv(runners.size() > 0, "No test to be run was found.");
+        } catch (JCUnitUserException e) {
+            e.setTargetClass(klass);
+            throw e;
+        }
+    }
 
-	private int registerLabeledTestCases(int id,
-	                                     TestCaseFilter filter,
-	                                     Factors factors,
-	                                     Iterable<LabeledTestCase> labeledTestCases,
-	                                     TestCaseType testCaseType)
-			throws InitializationError {
-		for (LabeledTestCase labeledTestCase : labeledTestCases) {
-			if (filter.shouldBeExecuted(id)) {
-				runners.add(new JCUnitRunner(getTestClass().getJavaClass(),
-						id, testCaseType, labeledTestCase.getLabels(),
-						factors,
-						labeledTestCase.getTestCase()));
-			}
-			id++;
-		}
-		return id;
-	}
+    private int registerLabeledTestCases(int id,
+                                         Factors factors,
+                                         Iterable<LabeledTestCase> labeledTestCases,
+                                         TestCaseType testCaseType)
+            throws InitializationError {
+        for (LabeledTestCase labeledTestCase : labeledTestCases) {
+            runners.add(new JCUnitRunner(getTestClass().getJavaClass(),
+                    id, testCaseType, labeledTestCase.getLabels(),
+                    factors,
+                    labeledTestCase.getTestCase()));
+            id++;
+        }
+        return id;
+    }
 
-	private TestCaseFilter createTestCaseFilter(Class<?> klass) {
-		if (klass.isAnnotationPresent(Execute.class)) {
-			return TestCaseFilter
-					.createTestCaseFilter(klass.getAnnotation(Execute.class));
-		} else {
-			return TestCaseFilter.createTestCaseFilter();
-		}
-	}
+    @Override
+    protected List<Runner> getChildren() {
+        return runners;
+    }
 
-	@Override
-	protected List<Runner> getChildren() {
-		return runners;
-	}
+    @SuppressWarnings("unchecked")
+    private Iterable<LabeledTestCase> allCustomTestCases() throws Throwable {
+        Object parameters = getParametersMethod().invokeExplosively(null);
+        if (parameters instanceof Iterable) {
+            return (Iterable<LabeledTestCase>) parameters;
+        } else {
+            throw parametersMethodReturnedWrongType();
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	private Iterable<LabeledTestCase> allCustomTestCases() throws Throwable {
-		Object parameters = getParametersMethod().invokeExplosively(null);
-		if (parameters instanceof Iterable) {
-			return (Iterable<LabeledTestCase>) parameters;
-		} else {
-			throw parametersMethodReturnedWrongType();
-		}
-	}
+    private boolean hasFilterMethod() {
+        List<FrameworkMethod> methods = getTestClass().getAnnotatedMethods(
+                Filter.class);
+        for (FrameworkMethod each : methods) {
+            if (each.isStatic() && each.isPublic()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private boolean hasParametersMethod() {
-		List<FrameworkMethod> methods = getTestClass().getAnnotatedMethods(
-				CustomTestCases.class);
-		for (FrameworkMethod each : methods) {
-			if (each.isStatic() && each.isPublic()) {
-				return true;
-			}
-		}
-		return false;
-	}
 
-	private FrameworkMethod getParametersMethod() throws Exception {
-		List<FrameworkMethod> methods = getTestClass().getAnnotatedMethods(
-				CustomTestCases.class);
-		for (FrameworkMethod each : methods) {
-			if (each.isStatic() && each.isPublic()) {
-				return each;
-			}
-		}
-		throw new Exception("No public static parameters method on class "
-				+ getTestClass().getName());
-	}
+    private boolean hasCustomTestCasesMethod() {
+        List<FrameworkMethod> methods = getTestClass().getAnnotatedMethods(
+                CustomTestCases.class);
+        for (FrameworkMethod each : methods) {
+            if (each.isStatic() && each.isPublic()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private Exception parametersMethodReturnedWrongType() throws Exception {
-		String className = getTestClass().getName();
-		String methodName = getParametersMethod().getName();
-		String message = MessageFormat.format(
-				"{0}.{1}() must return an Iterable of arrays.",
-				className, methodName);
-		return new Exception(message);
-	}
+    private FrameworkMethod getParametersMethod() throws Exception {
+        List<FrameworkMethod> methods = getTestClass().getAnnotatedMethods(
+                CustomTestCases.class);
+        for (FrameworkMethod each : methods) {
+            if (each.isStatic() && each.isPublic()) {
+                return each;
+            }
+        }
+        throw new Exception("No public static parameters method on class "
+                + getTestClass().getName());
+    }
 
-	static class TestCaseFilter {
-		private final int[] include;
-		private final int[] exclude;
+    private Exception parametersMethodReturnedWrongType() throws Exception {
+        String className = getTestClass().getName();
+        String methodName = getParametersMethod().getName();
+        String message = MessageFormat.format(
+                "{0}.{1}() must return an Iterable of arrays.",
+                className, methodName);
+        return new Exception(message);
+    }
 
-		TestCaseFilter(int[] include, int[] exclude) {
-			if (include != null) {
-				for (int i : include) {
-					Utils.checkcond(i >= 0,
-							"include mustn't contain negative index. (%d was found)", i);
-				}
-				this.include = Arrays.copyOf(include, include.length);
-				Arrays.sort(this.include);
-			} else {
-				this.include = null;
-			}
-			Utils.checknotnull(exclude);
-			for (int i : exclude) {
-				Utils.checkcond(i >= 0,
-						"exclude mustn't contain negative index. (%d was found)", i);
-			}
-			this.exclude = Arrays.copyOf(exclude, exclude.length);
-			Arrays.sort(this.exclude);
-		}
+    public static interface FrameworkMethodValidator {
+        public static final FrameworkMethodValidator CUSTOM_TESTCASES = new FrameworkMethodValidator() {
+            @Override
+            public boolean validate(FrameworkMethod m) {
+                Method mm = m.getMethod();
+                return m.isPublic() && m.isStatic() && mm.getParameterTypes().length == 0 &&
+                        (LabeledTestCase.class.isAssignableFrom(mm.getReturnType()) ||
+                                Collections.class.isAssignableFrom(mm.getReturnType()));
+            }
 
-		boolean shouldBeExecuted(int testCaseIndex) {
-			if (this.include == null) {
-				return Arrays.binarySearch(this.exclude, testCaseIndex) < 0;
-			}
-			return Arrays.binarySearch(this.include, testCaseIndex) >= 0
-					&& Arrays.binarySearch(this.exclude, testCaseIndex) < 0;
-		}
+            @Override
+            public Class<? extends Annotation> getAnnotation() {
+                return CustomTestCases.class;
+            }
 
-		public static TestCaseFilter createTestCaseFilter(Execute annotation) {
-			int[] included = annotation.include();
-			if (included.length == 1) {
-				if (included[0] == -1) {
-					included = null;
-				}
-			}
-			int[] excluded = annotation.exclude();
-			return new TestCaseFilter(included, excluded);
-		}
+            @Override
+            public String getDescription() {
+                return "public, static, no parameter, and returning 'LabeledTestCase' or an iterable of it";
+            }
+        };
+        public static final FrameworkMethodValidator TESTCASE_FILTER = new FrameworkMethodValidator() {
+            @Override
+            public boolean validate(FrameworkMethod m) {
+                Method mm = m.getMethod();
+                boolean ret = Boolean.class.isAssignableFrom(mm.getReturnType());
 
-		public static TestCaseFilter createTestCaseFilter() {
-			return new TestCaseFilter(null, new int[]{});
-		}
+                return m.isStatic() && m.isPublic() && ret;
+            }
 
-	}
+            @Override
+            public Class<? extends Annotation> getAnnotation() {
+                return Filter.class;
+            }
 
-	public static class TestCaseInternalAnnotation implements Annotation {
+            @Override
+            public String getDescription() {
+                return "public, static, returning boolean, parameters are ";
+            }
+        };
 
-		private final TestCaseType type;
-		private final List<Serializable> labels;
-		private final int id;
-		private Factors factors;
-		private Tuple testCase;
+        public boolean validate(FrameworkMethod m);
 
-		public TestCaseInternalAnnotation(int id, TestCaseType type,
-		                                  List<Serializable> labels, Factors factors, Tuple testCase) {
-			Utils.checknotnull(type);
-			Utils.checknotnull(labels);
-			this.id = id;
-			this.type = type;
-			this.labels = labels;
-			this.factors = factors;
-			this.testCase = testCase;
-		}
+        public Class<? extends Annotation> getAnnotation();
 
-		@Override
-		public Class<? extends Annotation> annotationType() {
-			return this.getClass();
-		}
+        public String getDescription();
+    }
 
-		public int getId() {
-			return this.id;
-		}
+    public static class TestCaseInternalAnnotation implements Annotation {
 
-		public TestCaseType getTestCaseType() {
-			return this.type;
-		}
+        private final TestCaseType type;
+        private final List<Serializable> labels;
+        private final int id;
+        private Factors factors;
+        private Tuple testCase;
 
-		public List<Serializable> getLabels() {
-			return Collections.unmodifiableList(this.labels);
-		}
+        public TestCaseInternalAnnotation(int id, TestCaseType type,
+                                          List<Serializable> labels, Factors factors, Tuple testCase) {
+            Utils.checknotnull(type);
+            Utils.checknotnull(labels);
+            this.id = id;
+            this.type = type;
+            this.labels = labels;
+            this.factors = factors;
+            this.testCase = testCase;
+        }
 
-		public Tuple getTestCase() {
-			return testCase;
-		}
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return this.getClass();
+        }
 
-		public Factors getFactors() {
-			return factors;
-		}
-	}
+        public int getId() {
+            return this.id;
+        }
 
-	public static enum TestCaseType {
-		Custom,
-		Generated,
-		Violation
-	}
+        public TestCaseType getTestCaseType() {
+            return this.type;
+        }
 
-	@Retention(RetentionPolicy.RUNTIME)
-	public static @interface Execute {
-		/**
-		 * If '-1' is the only element of this attribute, all the test cases will
-		 * be executed.
-		 */
-		public int[] include() default {-1};
+        public List<Serializable> getLabels() {
+            return Collections.unmodifiableList(this.labels);
+        }
 
-		public int[] exclude() default {};
-	}
+        public Tuple getTestCase() {
+            return testCase;
+        }
+
+        public Factors getFactors() {
+            return factors;
+        }
+    }
+
+    public static enum TestCaseType {
+        Custom,
+        Generated,
+        Violation
+    }
 }
