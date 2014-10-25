d5e0aa345d361cd225aaf449032306a97644e730
dakusui <dakusui@gmail.com>
2014-10-25 13:31:48 +0900

    Add an example for state machine test (initial commit)

---
+++ src/test/java/com/github/dakusui/jcunit/examples/dfsm/DFSMTest.java
@@ -0,0 +1,46 @@
+package com.github.dakusui.jcunit.examples.dfsm;
+
+import com.github.dakusui.jcunit.core.FactorField;
+
+public class DFSMTest {
+  public Object sut = new Object();
+  @FactorField
+  public Machine.Transition t0;
+
+  @FactorField
+  public Machine.Transition t1;
+  
+  @FactorField
+  public Machine.Transition t2;
+
+
+  public static class Machine {
+    public static class Expectation {
+      State s;
+      Object result;
+      Throwable t;
+    }
+    public static class Transition {
+      State s;
+      Symbol input;
+      Expectation expectation;
+    }
+    public static enum State {
+      s0 {
+        @Override
+        public State input(Symbol e) {
+          return s0;
+        }
+      };
+
+      public State input(Symbol e) {
+        throw new IllegalStateException();
+      };
+    }
+
+    public static enum Symbol {
+      s1,
+      s2;
+    }
+  }
+}
