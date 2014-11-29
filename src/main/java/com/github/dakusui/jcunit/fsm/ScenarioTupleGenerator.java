04d3db2f65ba789f1fe03266d788bc9c8ab1245e
dakusui <dakusui@gmail.com>
2014-11-29 18:32:59 +0900

    FSM support

--- src/main/java/com/github/dakusui/jcunit/fsm/ScenarioTupleGenerator.java
+++ src/main/java/com/github/dakusui/jcunit/fsm/ScenarioTupleGenerator.java
@@ -1,10 +1,13 @@
 package com.github.dakusui.jcunit.fsm;
 
+import com.github.dakusui.jcunit.constraint.ConstraintManager;
 import com.github.dakusui.jcunit.core.ParamType;
 import com.github.dakusui.jcunit.core.tuples.Tuple;
+import com.github.dakusui.jcunit.generators.IPO2TupleGenerator;
+import com.github.dakusui.jcunit.generators.TupleGenerator;
 import com.github.dakusui.jcunit.generators.TupleGeneratorBase;
 
-class ScenarioTupleGenerator<SUT> extends TupleGeneratorBase {
+public abstract class ScenarioTupleGenerator<SUT> extends TupleGeneratorBase {
   @Override
   public Tuple getTuple(int tupleId) {
     return null;
@@ -12,11 +15,32 @@ class ScenarioTupleGenerator<SUT> extends TupleGeneratorBase {
 
   @Override
   protected long initializeTuples(Object[] params) {
-    return 0;
+    int historyLength = (Integer)params[0];
+    int strength = (Integer)params[1];
+    FSMFactors factors = new FSMFactors.Builder<SUT>()
+        .setFSM(createFSM())
+        .setLength(historyLength)
+        .build();
+    ConstraintManager cm = new FSMConstraintManager<SUT>();
+    cm.setFactors(factors);
+    TupleGenerator tupleGenerator = new Builder()
+        .setConstraintManager(cm)
+        .setFactors(factors)
+        .setParameters(new Object[]{strength})
+        .setTargetClass(this.getTargetClass())
+        .setTupleGeneratorClass(IPO2TupleGenerator.class)
+        .build();
+    tupleGenerator.init(params);
+    return this.size();
   }
+
+  protected abstract FSM<SUT> createFSM();
 
   @Override
   public ParamType[] parameterTypes() {
-    return new ParamType[0];
+    return new ParamType[]{
+        ParamType.Int.withDefaultValue(2),
+        ParamType.Int.withDefaultValue(2)
+    };
   }
 }
