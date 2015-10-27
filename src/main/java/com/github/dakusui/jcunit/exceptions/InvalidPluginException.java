c89e33bbec7c6a903133c67b41802e853ec477ff
dakusui <dakusui@gmail.com>
2015-10-28 07:54:23 +0900

    Small code formatting.

--- src/main/java/com/github/dakusui/jcunit/exceptions/InvalidPluginException.java
+++ src/main/java/com/github/dakusui/jcunit/exceptions/InvalidPluginException.java
@@ -11,7 +11,8 @@ public class InvalidPluginException extends JCUnitException {
    *
    * @param message An error message for this object.
    * @param t       A nested <code>throwable</code> object.
-   */  public InvalidPluginException(String message, Throwable t) {
+   */
+  public InvalidPluginException(String message, Throwable t) {
     super(message, t);
   }
 
@@ -19,7 +20,8 @@ public class InvalidPluginException extends JCUnitException {
    * Creates an object of this class.
    *
    * @param message An error message for this object.
-   */  public InvalidPluginException(String message) {
+   */
+  public InvalidPluginException(String message) {
     super(message, null);
   }
 }
