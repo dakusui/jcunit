88c9cca4f2eed4217d5466da99b4b6c876598660
dakusui <dakusui@gmail.com>
2016-02-25 07:50:51 +0900

    Issue-#10: WIP

--- src/main/java/com/github/dakusui/jcunit/coverage/FSMMetrics.java
+++ src/main/java/com/github/dakusui/jcunit/coverage/FSMMetrics.java
@@ -2,6 +2,7 @@ package com.github.dakusui.jcunit.coverage;
 
 import com.github.dakusui.jcunit.core.Checks;
 import com.github.dakusui.jcunit.core.tuples.Tuple;
+import com.github.dakusui.jcunit.fsm.Action;
 import com.github.dakusui.jcunit.fsm.FSM;
 import com.github.dakusui.jcunit.fsm.ScenarioSequence;
 import com.github.dakusui.jcunit.fsm.State;
@@ -12,12 +13,23 @@ import java.util.Set;
 public class FSMMetrics extends Metrics.Base implements Metrics {
   private final FSM<?> targetFSM;
   private final int historyLength;
+  private final String targetFSMName;
 
-  public FSMMetrics(FSM<?> fsm, int historyLength) {
+  public FSMMetrics(String fsmName, FSM<?> fsm, int historyLength) {
+    Checks.checknotnull(fsmName);
     Checks.checknotnull(fsm);
     Checks.checkcond(historyLength > 0);
+    this.targetFSMName = fsmName;
     this.targetFSM = fsm;
     this.historyLength = historyLength;
+  }
+
+  private ScenarioSequence createScenarioSequenceFromTuple(Tuple tuple) {
+    return new ScenarioSequence.BuilderFromTuple()
+        .setTuple(tuple)
+        .setFSMName(this.targetFSMName)
+        .setHistoryLength(FSMMetrics.this.historyLength)
+        .build();
   }
 
   @Metrics.Base.MetricItem
@@ -25,11 +37,7 @@ public class FSMMetrics extends Metrics.Base implements Metrics {
     return new Metric.CoverageMetric<State>(new HashSet<State>(targetFSM.states())) {
       @Override
       protected Set<State> getCoveredItemsBy(Tuple tuple) {
-        ScenarioSequence scenarioSequence = new ScenarioSequence.BuilderFromTuple()
-            .setTuple(tuple)
-            .setFSMName(this.name())
-            .setHistoryLength(FSMMetrics.this.historyLength)
-            .build();
+        ScenarioSequence scenarioSequence = createScenarioSequenceFromTuple(tuple);
         Set<State> ret = new HashSet<State>();
         for (int i = 0; i < scenarioSequence.size(); i++) {
           ret.add(scenarioSequence.get(i).given);
@@ -40,6 +48,26 @@ public class FSMMetrics extends Metrics.Base implements Metrics {
       @Override
       public String name() {
         return "State coverage";
+      }
+    };
+  }
+
+  @Metrics.Base.MetricItem
+  public Metric.CoverageMetric actionCoverage() {
+    return new Metric.CoverageMetric<Action>(new HashSet<Action>(targetFSM.actions())) {
+      @Override
+      protected Set<Action> getCoveredItemsBy(Tuple tuple) {
+        ScenarioSequence scenarioSequence = createScenarioSequenceFromTuple(tuple);
+        Set<Action> ret = new HashSet<Action>();
+        for (int i = 0; i < scenarioSequence.size(); i++) {
+          ret.add(scenarioSequence.get(i).when);
+        }
+        return ret;
+      }
+
+      @Override
+      public String name() {
+        return "Action coverage";
       }
     };
   }
