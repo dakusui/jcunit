3f2516cde71de31ceba574bf067578a1ec385464
Hiroshi Ukai <dakusui@gmail.com>
2017-04-30 04:31:45 +0900

    remove old files

--- src/site/archive/test/com/github/dakusui/jcunit/tests/features/fsm/nestedfsm/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Tests under this package verifies 'nested fsm' feature of JCUnit.
- */
-package com.github.dakusui.jcunit.tests.features.fsm.nestedfsm;
--- src/site/archive/test/com/github/dakusui/jcunit/testutils/tuples/Expectation.java
+++
@@ -1,9 +0,0 @@
-package com.github.dakusui.jcunit.testutils.tuples;
-
-import com.github.dakusui.jcunit.core.tuples.Tuple;
-
-import java.util.List;
-
-public interface Expectation {
-  VerificationResult verify(List<Tuple> tuples);
-}
