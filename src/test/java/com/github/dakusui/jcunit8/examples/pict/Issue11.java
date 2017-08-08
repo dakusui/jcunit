a5b7f5c8b22e45a0518e7d7323180244dc398cdb
UKAI Hiroshi <hiroshi.ukai@rakuten.com>
2017-08-08 20:24:40 +0900

    Add examples that do same things reported in pict's project. Support 'sequence' parameter type. (initial implementation)

--- src/test/java/com/github/dakusui/jcunit8/examples/pict/Issue11.java
+++ src/test/java/com/github/dakusui/jcunit8/examples/pict/Issue11.java
@@ -1,5 +1,15 @@
 package com.github.dakusui.jcunit8.examples.pict;
 
+import com.github.dakusui.jcunit8.factorspace.Parameter;
+import com.github.dakusui.jcunit8.runners.helpers.Parameters;
+import com.github.dakusui.jcunit8.runners.junit4.JCUnit8;
+import com.github.dakusui.jcunit8.runners.junit4.annotations.From;
+import com.github.dakusui.jcunit8.runners.junit4.annotations.ParameterSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
 /**
  * <a href="https://github.com/Microsoft/pict/issues/11">Issue-11 of PICT</a>
  * <code>
@@ -34,5 +44,56 @@ package com.github.dakusui.jcunit8.examples.pict;
  * For a list the above example would not have [APPS_0] <> [APPS_1] and would have APPS (List) instead of APPS (Set) (or some similar grammar).
  * </code>
  */
+@RunWith(JCUnit8.class)
 public class Issue11 {
+  @ParameterSource
+  public Parameter.Factory<String> platform() {
+    return Parameters.simple("x86", "ia16", "amd64");
+  }
+
+  @ParameterSource
+  public Parameter.Factory<String> cpu() {
+    return Parameters.simple("single", "dual", "quad");
+  }
+
+  @ParameterSource
+  public Parameter.Factory<String> ram() {
+    return Parameters.simple("128MB", "1GB", "4GB", "64GB");
+  }
+
+  @ParameterSource
+  public Parameter.Factory<String> hdd() {
+    return Parameters.simple("SCSI", "IDE");
+  }
+
+  @ParameterSource
+  public Parameter.Factory<String> os() {
+    return Parameters.simple("NT4", "Win2K", "WinXP", "Win2K3");
+  }
+
+  @ParameterSource
+  public Parameter.Factory<String> ie() {
+    return Parameters.simple("4.0", "5.0", "5.5", "6.0");
+  }
+
+  @ParameterSource
+  public Parameter.Factory<List<String>> apps() {
+    return Parameters.sequence("SQLServer", "Exchange", "Office").size(3, 4).withoutRepetition().build();
+  }
+
+  @Test
+  public void test(
+      /*/
+      @From("platform") String platform,
+      @From("cpu") String cpu,
+      @From("ram") String ram,
+      @From("hdd") String hdd,
+      @From("os") String os,
+      @From("ie") String ie,
+      /*/
+      @From("apps") List<Object> app
+  ) {
+    System.out.println(app);
+    //System.out.println(String.format("%s,%s,%s,%s,%s,%s,%s", platform, cpu, ram, hdd, os, ie, app));
+  }
 }
