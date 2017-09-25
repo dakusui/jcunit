d189ddcdc119be867a3e50f3d80314870751c53b
Hiroshi Ukai <dakusui@gmail.com>
2017-09-26 07:25:22 +0900

    Add check write for 'before' test oracles.

--- src/test/java/com/github/dakusui/jcunit8/sandbox/JCUnit8XTest.java
+++ src/test/java/com/github/dakusui/jcunit8/sandbox/JCUnit8XTest.java
@@ -1,21 +1,29 @@
 package com.github.dakusui.jcunit8.sandbox;
 
+import com.github.dakusui.jcunit.core.tuples.Tuple;
 import com.github.dakusui.jcunit8.examples.executionsequence.ExampleParameterSpace;
 import com.github.dakusui.jcunit8.pipeline.stages.ConfigFactory;
 import com.github.dakusui.jcunit8.runners.junit4.JCUnit8X;
 import com.github.dakusui.jcunit8.testsuite.TestCase;
+import com.github.dakusui.jcunit8.testsuite.TestSuite;
 import org.junit.Test;
 import org.junit.runners.model.TestClass;
+
+import java.util.function.Consumer;
 
 public class JCUnit8XTest {
   @Test
   public void givenTestClass$whenBuildTestSuite$thenBuilt() {
-    for (TestCase each : JCUnit8X.buildTestSuite(
+    TestSuite testSuite = JCUnit8X.buildTestSuite(
         new TestClass(JCUnit8XExample.class),
         new TestClass(ExampleParameterSpace.class),
         new ConfigFactory.Default()
-    )) {
+    );
+    for (TestCase each : testSuite) {
       System.out.println(each);
+    }
+    for (Consumer<Tuple> beforeTestOracle : testSuite.beforeTestOracle()) {
+      System.out.println(beforeTestOracle);
     }
   }
 }
