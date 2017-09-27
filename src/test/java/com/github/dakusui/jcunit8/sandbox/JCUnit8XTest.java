ec12fdccd5df6d9f9900ac276095a358f969a2c9
UKAI Hiroshi <hiroshi.ukai@rakuten.com>
2017-09-28 08:14:31 +0900

    Refine unit tests

--- src/test/java/com/github/dakusui/jcunit8/sandbox/JCUnit8XTest.java
+++ src/test/java/com/github/dakusui/jcunit8/sandbox/JCUnit8XTest.java
@@ -1,6 +1,6 @@
 package com.github.dakusui.jcunit8.sandbox;
 
-import com.github.dakusui.jcunit.core.tuples.Tuple;
+import com.github.dakusui.crest.core.Printable;
 import com.github.dakusui.jcunit8.examples.executionsequence.ExampleParameterSpace;
 import com.github.dakusui.jcunit8.pipeline.stages.ConfigFactory;
 import com.github.dakusui.jcunit8.runners.junit4.JCUnit8X;
@@ -9,21 +9,63 @@ import com.github.dakusui.jcunit8.testsuite.TestSuite;
 import org.junit.Test;
 import org.junit.runners.model.TestClass;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+
+import static com.github.dakusui.crest.Crest.asListOf;
+import static com.github.dakusui.crest.Crest.assertThat;
+import static com.github.dakusui.crest.functions.CrestFunctions.size;
+import static com.github.dakusui.crest.functions.CrestPredicates.equalTo;
 
 public class JCUnit8XTest {
   @Test
-  public void givenTestClass$whenBuildTestSuite$thenBuilt() {
-    TestSuite testSuite = JCUnit8X.buildTestSuite(
-        new TestClass(JCUnit8XExample.class),
-        new TestClass(ExampleParameterSpace.class),
+  public void givenTestClass$whenBuildTestSuite$thenTestCasesBuilt() {
+    assertThat(
+        buildTestSuite(),
+        asListOf(
+            TestCase.class
+        ).check(
+            size(),
+            equalTo(2)
+        ).$()
+    );
+  }
+
+  @Test
+  public void givenTestClass$whenBuildTestSuite$thenConsumersRunBeforeTestOracleBuilt() {
+    assertThat(
+        buildTestSuite(),
+        asListOf(
+            Consumer.class,
+            Printable.function(
+                "toConsumerListWithBeforeTestOracleMethod",
+                toList(TestSuite::beforeTestOracle))
+        ).check(
+            size(),
+            equalTo(1)
+        ).$()
+    );
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <I, O> Function<I, List<O>> toList(Function<I, List<?>> toListFunc) {
+    return i -> new LinkedList<O>() {{
+      addAll((Collection<? extends O>) toListFunc.apply(i));
+    }};
+  }
+
+  private TestSuite buildTestSuite() {
+    return buildTestSuite(JCUnit8XExample.class, ExampleParameterSpace.class);
+  }
+
+  private static TestSuite buildTestSuite(Class javaTestClass, Class javaParameterSpaceClass) {
+    return JCUnit8X.buildTestSuite(
+        new TestClass(javaTestClass),
+        new TestClass(javaParameterSpaceClass),
         new ConfigFactory.Default()
     );
-    for (TestCase each : testSuite) {
-      System.out.println(each);
-    }
-    for (Consumer<Tuple> beforeTestOracle : testSuite.beforeTestOracle()) {
-      System.out.println(beforeTestOracle);
-    }
   }
 }
