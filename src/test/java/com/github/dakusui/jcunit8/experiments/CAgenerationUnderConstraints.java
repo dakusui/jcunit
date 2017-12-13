0214c3222860886b16c58af1766a3b0c06a22838
Hiroshi Ukai <dakusui@gmail.com>
2017-12-14 08:43:48 +0900

    Update join test

--- src/test/java/com/github/dakusui/jcunit8/experiments/CAgenerationUnderConstraints.java
+++ src/test/java/com/github/dakusui/jcunit8/experiments/CAgenerationUnderConstraints.java
@@ -9,11 +9,13 @@ import com.github.dakusui.jcunit8.pipeline.Requirement;
 import com.github.dakusui.jcunit8.pipeline.stages.Joiner;
 import com.github.dakusui.jcunit8.runners.helpers.ParameterUtils;
 import com.github.dakusui.jcunit8.testsuite.SchemafulTupleSet;
+import com.github.dakusui.jcunit8.testsuite.TestCase;
 import com.github.dakusui.jcunit8.testsuite.TestSuite;
 import com.github.dakusui.jcunit8.testutils.testsuitequality.FactorSpaceSpec;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.github.dakusui.jcunit8.testutils.testsuitequality.CoveringArrayGenerationUtils.generateWithIpoGplus;
 import static java.util.Arrays.asList;
@@ -98,44 +100,65 @@ public class CAgenerationUnderConstraints {
 
   @Test
   public void c10$2_pipelined() {
+    int strength = 2;
+    SchemafulTupleSet lhs = buildSchemafulTupleSet("p0", strength);
+    SchemafulTupleSet rhs = buildSchemafulTupleSet("p1", strength);
     long before = System.currentTimeMillis();
-    Requirement requirement = new Requirement.Builder().withStrength(3).build();
-    TestSuite testSuite = Pipeline.Standard.create().execute(
-        new Config.Builder(requirement).withJoiner(new CustomJoiner(requirement)).build(),
+    System.out.printf("lhs size=%s%n", lhs.size());
+    System.out.printf("rhs size=%s%n", rhs.size());
+    System.out.println("joining");
+    SchemafulTupleSet result = new CustomJoiner(new Requirement.Builder().withStrength(strength).build()).doJoin(lhs, rhs);
+    long after = System.currentTimeMillis();
+    System.out.printf("size=%s;time=%s[msec]%n", result.size(), after - before);
+  }
+
+  @Test
+  public void c10$3_pipelined() {
+    int strength = 2;
+    SchemafulTupleSet lhs = buildSchemafulTupleSet("p0", strength);
+    SchemafulTupleSet rhs = buildSchemafulTupleSet("p1", strength);
+    SchemafulTupleSet rhs2 = buildSchemafulTupleSet("p2", strength);
+    long before = System.currentTimeMillis();
+    System.out.printf("lhs size=%s%n", lhs.size());
+    System.out.printf("rhs size=%s%n", rhs.size());
+    System.out.println("joining");
+    SchemafulTupleSet joined1 = new CustomJoiner(new Requirement.Builder().withStrength(strength).build()).doJoin(lhs, rhs);
+    long after = System.currentTimeMillis();
+    System.out.printf(">>>size=%s;time=%s[msec]%n", joined1.size(), after - before);
+
+    long before2 = System.currentTimeMillis();
+    System.out.printf("lhs size=%s%n", joined1.size());
+    System.out.printf("rhs size=%s%n", rhs2.size());
+    System.out.println("joining");
+    SchemafulTupleSet result = new CustomJoiner(new Requirement.Builder().withStrength(strength).build()).doJoin(joined1, rhs2);
+    long after2 = System.currentTimeMillis();
+    System.out.printf(">>>size=%s;time=%s[msec]%n", result.size(), after2 - before2);
+  }
+
+  private SchemafulTupleSet buildSchemafulTupleSet(String prefix, int strength) {
+    Requirement requirement = new Requirement.Builder().withStrength(strength).build();
+    return toSchemafulTupleSet(Pipeline.Standard.create().execute(
+        new Config.Builder(
+            requirement
+        ).withJoiner(new CustomJoiner(requirement)).build(),
         new ParameterSpace.Builder()
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p00"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p01"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p02"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p03"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p04"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p05"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p06"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p07"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p08"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p09"))
-            .addConstraint(constraint("p01", "p02"))
-            .addConstraint(constraint("p03", "p04"))
-            .addConstraint(constraint("p05", "p06"))
-            .addConstraint(constraint("p07", "p08"))
-            .addConstraint(constraint("p09", "p02"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p10"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p11"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p12"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p13"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p14"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p15"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p16"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p17"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p18"))
-            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p19"))
-            .addConstraint(constraint("p11", "p12"))
-            .addConstraint(constraint("p13", "p14"))
-            .addConstraint(constraint("p15", "p16"))
-            .addConstraint(constraint("p17", "p18"))
-            .addConstraint(constraint("p19", "p12"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "0"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "1"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "2"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "3"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "4"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "5"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "6"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "7"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "8"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create(prefix + "9"))
+            .addConstraint(constraint(prefix + "1", prefix + "2"))
+            .addConstraint(constraint(prefix + "3", prefix + "4"))
+            .addConstraint(constraint(prefix + "5", prefix + "6"))
+            .addConstraint(constraint(prefix + "7", prefix + "8"))
+            .addConstraint(constraint(prefix + "9", prefix + "2"))
             .build()
-    );
-    System.out.printf("lines(c10_pipelined)=%s;%d[msec]%n", testSuite.size(), System.currentTimeMillis() - before);
+    ));
   }
 
   private Constraint constraint(String a, String b) {
@@ -155,6 +178,14 @@ public class CAgenerationUnderConstraints {
         return asList(a, b);
       }
     };
+  }
+
+  private SchemafulTupleSet toSchemafulTupleSet(TestSuite testSuite) {
+    return new SchemafulTupleSet.Builder(
+        testSuite.getParameterSpace().getParameterNames()
+    ).addAll(
+        testSuite.stream().map(TestCase::get).collect(Collectors.toList())
+    ).build();
   }
 
   public static class CustomJoiner extends Joiner.Standard {
