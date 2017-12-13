74b82d15662b242e00ee75cf94ae30f9fe479100
Hiroshi Ukai <dakusui@gmail.com>
2017-12-14 06:55:47 +0900

    Add a new (and more complex) experiment

--- src/test/java/com/github/dakusui/jcunit8/experiments/CAgenerationUnderConstraints.java
+++ src/test/java/com/github/dakusui/jcunit8/experiments/CAgenerationUnderConstraints.java
@@ -96,6 +96,48 @@ public class CAgenerationUnderConstraints {
     System.out.printf("lines(c10_pipelined)=%s;%d[msec]%n", testSuite.size(), System.currentTimeMillis() - before);
   }
 
+  @Test
+  public void c10$2_pipelined() {
+    long before = System.currentTimeMillis();
+    Requirement requirement = new Requirement.Builder().withStrength(3).build();
+    TestSuite testSuite = Pipeline.Standard.create().execute(
+        new Config.Builder(requirement).withJoiner(new CustomJoiner(requirement)).build(),
+        new ParameterSpace.Builder()
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p00"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p01"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p02"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p03"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p04"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p05"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p06"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p07"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p08"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p09"))
+            .addConstraint(constraint("p01", "p02"))
+            .addConstraint(constraint("p03", "p04"))
+            .addConstraint(constraint("p05", "p06"))
+            .addConstraint(constraint("p07", "p08"))
+            .addConstraint(constraint("p09", "p02"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p10"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p11"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p12"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p13"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p14"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p15"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p16"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p17"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p18"))
+            .addParameter(ParameterUtils.simple(0, 1, 2, 3).create("p19"))
+            .addConstraint(constraint("p11", "p12"))
+            .addConstraint(constraint("p13", "p14"))
+            .addConstraint(constraint("p15", "p16"))
+            .addConstraint(constraint("p17", "p18"))
+            .addConstraint(constraint("p19", "p12"))
+            .build()
+    );
+    System.out.printf("lines(c10_pipelined)=%s;%d[msec]%n", testSuite.size(), System.currentTimeMillis() - before);
+  }
+
   private Constraint constraint(String a, String b) {
     return new Constraint() {
       @Override
