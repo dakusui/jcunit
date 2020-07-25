157f62bb9e03a7cd73c0955631f194636d00a6e6
Hiroshi Ukai <dakusui@gmail.com>
2020-07-26 08:11:07 +0900

    Address warnings in TestDefinitionException.java

--- src/main/java/com/github/dakusui/jcunit8/exceptions/TestDefinitionException.java
+++ src/main/java/com/github/dakusui/jcunit8/exceptions/TestDefinitionException.java
@@ -4,14 +4,12 @@ import com.github.dakusui.jcunit.core.tuples.Tuple;
 import com.github.dakusui.jcunit8.factorspace.Constraint;
 
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 import static java.lang.String.format;
 
 /**
  * Indicates user's artifact (Typically test classes, annotated with
- * {@literal @}{@code Runwith(JCUnit.class)}), is invalid.
+ * {@literal @}{@code RunWith(JCUnit.class)}), is invalid.
  */
 public class TestDefinitionException extends BaseException {
   private TestDefinitionException(String message) {
@@ -30,32 +28,8 @@ public class TestDefinitionException extends BaseException {
     throw new TestDefinitionException(format("Constraints '%s' did not become true.", constraints));
   }
 
-  public static TestDefinitionException fsmDoesNotHaveRouteToSpecifiedState(Object destinationState, String fsmName, Object fsmModel) {
-    throw new TestDefinitionException(format("No route to '%s' was found on FSM:'%s'(%s)", destinationState, fsmName, fsmModel));
-  }
-
   public static TestDefinitionException failedToCover(String factorName, List<Object> factorLevels, Tuple tuple) {
     throw new TestDefinitionException(format("Factor '%s' doesn't have any valid level '%s' for tuple '%s'", factorName, factorLevels, tuple));
-  }
-
-  public static <T> T checkValue(T value, Predicate<T> check) {
-    return checkValue(value, check, "'%s' is not valid", value);
-  }
-
-  public static <T> T checkValue(T value, Predicate<T> check, Supplier<String> messageComposer) {
-    if (check.test(value))
-      return value;
-    throw new TestDefinitionException(messageComposer.get());
-  }
-
-  public static <T> T checkValue(T value, Predicate<T> check, String fmt, Object... args) {
-    return checkValue(value, check, () -> format(fmt, args));
-  }
-
-  public static Supplier<TestDefinitionException> sutDoesNotHaveSpecifiedMethod(Class<?> klass, String name, List args) {
-    return () -> {
-      throw new TestDefinitionException(format("SUT '%s' does not have a method %s/%d (%s)", klass.getCanonicalName(), name, args.size(), args));
-    };
   }
 
   public static TestDefinitionException parameterWithoutAnnotation(String methodName) {
