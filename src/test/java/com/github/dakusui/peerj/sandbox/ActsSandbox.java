5373078d7346498f349c461f03f2f766cf4fcb1b
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2021-03-02 04:42:50 +0900

    Add test results

---
+++ src/test/java/com/github/dakusui/peerj/sandbox/ActsSandbox.java
@@ -0,0 +1,11 @@
+package com.github.dakusui.peerj.sandbox;
+
+import edu.uta.cse.fireeye.console.ActsConsoleManager;
+import org.junit.Test;
+
+public class ActsSandbox {
+  @Test
+  public void runActs() throws Exception {
+    ActsConsoleManager.main(new String[0]);
+  }
+}
